b8e8f86ee2b9020c9ba1b2b6f667842005f0a5f9
George Motter <g6@themotters.com>
2018-01-02 19:49:13 -0800

    prune

---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter A Number: ");
+            double num = Convert.ToDouble( Console.ReadLine() );
+            Console.Write("Thanks. Now Enter A Number: ");
+            Char letter = Convert.ToChar(Console.ReadLine());
+            if(num >= 6)
+            {
+                Console.WriteLine("\nNumber Exceeds 5");
+                if(letter == 'c')
+                {
+                    Console.WriteLine("letter is 'c'");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nNumber Is 5 Or Less");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Pick a Number between 1-7: ");
+            int num = Convert.ToInt16(Console.ReadLine());
+            string day;
+            switch(num)
+            {
+                case 1:day = "Monday"; break;
+                case 2:day = "Tuesday"; break;
+                case 3:day = "Wednesday"; break;
+                case 4:day = "Thursday"; break;
+                case 5:day = "Friday"; break;
+                default:day = "Weekend Day"; break;
+            }
+            Console.WriteLine("Day " + num + ": " + day);
+            Console.ReadKey();
+        }
+    }
+}
